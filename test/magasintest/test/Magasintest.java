7342dad42e9870dacc1badbaf8ba919907c093d6
yveg <gambaly@gmail.com>
2016-09-21 16:30:13 +0200

    streaming final

--- test/magasintest/test/Magasintest.java
+++ test/magasintest/test/Magasintest.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 import magasinonline.Categorie;
+import magasinonline.Client;
 import magasinonline.Produit;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -49,10 +50,38 @@ public class Magasintest {
         
         Categorie caty = em.find(Categorie.class, 52L); //suffixer avec L la valeur 
         //System.out.println(caty.getProduits());
-        for(Produit p: caty.getProduits()) {
+        for(Produit p: caty.getProduits()){
             System.out.println(p);
         }
         
     }
-
+   
+    @Test
+    public void listeclientcommandtest() {
+        EntityManager em = Persistence.createEntityManagerFactory("MagasinOnlinePU").createEntityManager();
+    em.getTransaction().begin();
+        Client c1 = new Client();
+        
+        c1.setId(1L);
+        //on insert dans l'objet
+        em.persist(c1);
+         /*
+        Categorie c2 = new Categorie();
+        c1.setNom("Lunette de soleil");        
+        //on insert dans l'objet
+        em.persist(c2);
+        
+        Produit rayban = new Produit();
+        rayban.setCategorie(c2);
+        em.persist(rayban);
+        
+        //apres on commit 
+        em.getTransaction().commit();
+    
+        Client cl =em.find(Client.class,1L);
+        for(Commande c: cl.getCommande()) {
+        System.out(println(c);
+        }
+    }
+*/
 }
